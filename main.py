4f3a8290d45dff17bb6ae3783457e54324bef529
Yvan <varelaivan30@gmail.com>
2022-02-15 13:46:44 -0300

    Cookies y redireccionar

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
-app = Flask(__name__)
+app = Flask(__name__)  # Instancia de la app
 
 
+# Generar cookies de un usuario
 @app.route('/')
+def index():
+    userIp = request.remote_addr  # remote_addr -> Trae el ip del usuario
+    response = make_response(redirect('/hello')) # Redirecciona a la ruta hello
+    response.set_cookie('userIp', userIp) # vamos a regresar la ip del usuario
+
+    return response
+
+
+@app.route('/hello')  # Ruta de acceso con un decorador
 def hello():
-    return 'Hello word python'
+    userIp = request.cookies.get('userIp')
+    return f'Tu ip es: {userIp}'
 
 
+# Debug del servidor
 if __name__ == '__main__':
-    app.run(host='localhost', debug=True)
+    app.run(host='localhost', port=4000, debug=True)
