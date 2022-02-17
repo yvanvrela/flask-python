72fb98a005b165c4e516292b05d15ca9f63a9dce
Yvan <varelaivan30@gmail.com>
2022-02-17 18:13:50 -0300

    Organizacion de carpetas, LoginForm

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from logging import exception
 from flask import Flask, request, make_response, redirect, render_template, abort, session
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)  # Instancia de la app
 
 app.config['SECRET_KEY'] = 'SECRETO'
 
 todos = ['Comprar leche', 'Hacer todo de vuelta', 'Pasar el curso de flask']
+
+# Clase para los login
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[
+                           DataRequired()])  # Validar los datos
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 
 """ Manejos de errores"""
@@ -18,7 +30,8 @@ def not_found(error):
 
 @app.errorhandler(500)
 def not_found(error):
-    return render_template('500.html', error=error) # se debe usar una variable abort o que nada funcione
+    # se debe usar una variable abort o que nada funcione
+    return render_template('500.html', error=error)
 
 
 @app.route('/')
@@ -26,23 +39,26 @@ def index():
     userIp = request.remote_addr  # remote_addr -> Trae el ip del usuario
     # Redirecciona a la ruta hello
     response = make_response(redirect('/hello'))
-    #response.set_cookie('userIp', userIp)  # vamos a regresar la ip del usuario
+    # response.set_cookie('userIp', userIp)  # vamos a regresar la ip del usuario
     session['userIp'] = userIp
 
     return response
 
 
-@app.route('/hello')  # Ruta de acceso con un decorador
+# Ruta de acceso con un decorador
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    userIp = session.get('userIp') # Guardar variables encriptadas
+    userIp = session.get('userIp')  # Guardar variables encriptadas
+    loginForm = LoginForm()
     context = {
         'userIp': userIp,
-        'todos': todos
+        'todos': todos,
+        'loginForm': loginForm
     }
     # doble asterisco expande todas las variables
     return render_template('hello.html', **context)
 
 
 # # Debug del servidor
-# if __name__ == '__main__':
-#     app.run(host='localhost', port=4000, debug=True)
+if __name__ == '__main__':
+    app.run(host='localhost', port=4000, debug=True)
