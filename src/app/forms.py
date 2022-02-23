80afab21950780f2079452630ea47c52d2c9146c
Yvan <varelaivan30@gmail.com>
2022-02-22 21:25:26 -0300

    Estructuracion de carpetas, statics, templates movidos a 'app', se creo forms con la clase login

---
+++ src/app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+# Clase para los login
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[
+                           DataRequired()])  # Validar los datos
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
